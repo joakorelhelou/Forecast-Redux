cf8ce29fbb6f1e7f02aea33000f037f877a9a996
joakorelhelou <joako.33k@gmail.com>
2017-08-03 16:59:42 -0300

    58. Redux-Promise in practice

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
         this.state = { term: '' };
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+
     }
 
     onInputChange(event) {
@@ -17,6 +22,9 @@ export default class SearchBar extends Component {
         event.preventDefault();
 
         // We need to feth weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' })
+
     }
 
     render() {
@@ -35,3 +43,10 @@ export default class SearchBar extends Component {
         )
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+//null because we don't need state here
+export default connect(null, mapDispatchToProps)(SearchBar);
