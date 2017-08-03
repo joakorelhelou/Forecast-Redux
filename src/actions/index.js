82d26cec64a81bb11aac375b5e3970e76cec6eb1
joakorelhelou <joako.33k@gmail.com>
2017-08-03 16:05:17 -0300

    57. Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,16 @@
-const API_KEY = 'b6200133a6079e2f074e79239783b329';
+import axios from 'axios';
 
+const API_KEY = 'b6200133a6079e2f074e79239783b329';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
