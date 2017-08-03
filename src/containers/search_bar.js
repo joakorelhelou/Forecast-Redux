66e021188a8aa056c5fe893658216134d57bb353
joakorelhelou <joako.33k@gmail.com>
2017-08-03 14:58:11 -0300

    54. Form Elements in React

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -13,9 +13,15 @@ export default class SearchBar extends Component {
         this.setState({ term: event.target.value });
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // We need to feth weather data
+    }
+
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input
                     placeholder="Get a five-day forcast in your favorite cities"
                     className="form-control"
